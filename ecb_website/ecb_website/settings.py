99d8e4d5a7721ab0d349b10cac5bc4b49f77e985
GabrielConner <gabriel.conner2008@gmail.com>
2026-04-08 18:21:32 -0600

    Update allowed addresses

--- ecb_website/ecb_website/settings.py
+++ ecb_website/ecb_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8$wmusjd_qr$-))agw-75j+ojpdmm2%3^v-e#l(bslppls3504
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','3.83.45.207']
 
 
 # Application definition
