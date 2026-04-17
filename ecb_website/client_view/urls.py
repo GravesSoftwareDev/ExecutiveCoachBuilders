431ac55ccc560a1301f7719a0ebea686622b2db9
gabrielconner <gabriel.conner2008@gmail.com>
2026-04-16 23:03:42 -0500

    About and Contact sites

--- ecb_website/client_view/urls.py
+++ ecb_website/client_view/urls.py
@@ -5,4 +5,6 @@ app_name = 'client_view'
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('about/', views.about, name = 'about'),
+    path('contact/', views.contact, name = 'contact'),
 ]
--- ecb_website/client_view/views.py
+++ ecb_website/client_view/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'client_view/home.html')
+
+
+
+def about(request):
+    return render(request, 'client_view/about.html')
+
+
+
+def contact(request):
+    return render(request, 'client_view/contact.html')
